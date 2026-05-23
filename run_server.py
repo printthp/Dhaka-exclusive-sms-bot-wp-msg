ed0089ca16681cadbb1e5f05e715cf7e2c021b70
printthp <print.thp@gmail.com>
2026-05-24 03:11:20 +0600

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,13 +1,7 @@
+from SMS_BOT import app
 import os
-# প্রথমে মেইন ফাইল ইম্পোর্ট করুন
-import SMS_BOT
-# এরপর আপনার এক্সটেনশন ফাইল ইম্পোর্ট করুন
-import bot_extensions
-
-# মেইন ফাইলের অ্যাপটিকে ধরুন
-application = SMS_BOT.application
 
 if __name__ == "__main__":
-    # পোর্ট ঠিক রেখে সার্ভার চালু করুন
+    # Gunicorn বা সরাসরি রান করার জন্য উপযোগী
     port = int(os.environ.get("PORT", 5000))
-    application.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
