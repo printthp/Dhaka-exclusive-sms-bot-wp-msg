816a43d1aff0aaee6985c404191b0891514f68b0
printthp <print.thp@gmail.com>
2026-05-24 03:18:16 +0600

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -1,7 +1,4 @@
-from SMS_BOT import app
-import os
-
-if __name__ == "__main__":
-    # Gunicorn বা সরাসরি রান করার জন্য উপযোগী
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+Flask
+gunicorn
+google-generativeai
+requests
