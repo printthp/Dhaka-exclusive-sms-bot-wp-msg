ed29b1c21ebcc73e9ff9992d2d237f6e9d6507b4
printthp <print.thp@gmail.com>
2026-05-24 03:28:01 +0600

    Update bot_extensions.py

--- bot_extensions.py
+++ bot_extensions.py
@@ -1,7 +1,15 @@
-def update_system_access(agent_id, level):
-    """ড্যাশবোর্ড থেকে এক্সেস চেঞ্জ করার জন্য"""
-    conn = sqlite3.connect("bot_v7_ultimate.db")
+#def update_system_access(agent_id, level):
+    #"""ড্যাশবোর্ড থেকে এক্সেস চেঞ্জ করার জন্য"""
+     #conn = sqlite3.connect("bot_v7_ultimate.db")
+      #c = conn.cursor()
+     #c.execute("UPDATE agents SET access_level=? WHERE id=?", (level, agent_id))
+     #conn.commit()
+     #conn.close()
+
+def update_dynamic_access(agent_id, access_payload):
+    conn = sqlite3.connect(SMS_BOT.DB_FILE)
     c = conn.cursor()
-    c.execute("UPDATE agents SET access_level=? WHERE id=?", (level, agent_id))
+    c.execute("UPDATE agents SET permissions=? WHERE id=?", (json.dumps(access_payload), agent_id))
     conn.commit()
     conn.close()
+    return True
